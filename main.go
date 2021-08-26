c0e70988219a3cb79d265a42ce8678b33cb7f782
chawit033 <61050033@kmitl.ac.th>
2021-08-26 20:27:10 +0700

    Songs-Template

--- main.go
+++ main.go
@@ -15,6 +15,15 @@ type Menu struct {
 	Price       int
 }
 
+type Song struct {
+	Id       int
+	SongName string
+	Band     string
+	Artist   string
+	Album    string
+	Genre    string
+}
+
 var menus = []Menu{
 	{
 		Id:          1,
@@ -33,6 +42,44 @@ var menus = []Menu{
 		Name:        "Jewel Meat",
 		Description: "Incandescent lamp-like radiance that dulls jewels and lights up a night sky",
 		Price:       8000,
+	},
+}
+
+var songs = []Song{
+	{
+		Id:       1,
+		SongName: "Whiplash",
+		Band:     "Metallica",
+		Artist:   "James Hetfield",
+		Album:    "KILL'EM ALL",
+		Genre:    "Metal",
+	},
+
+	{
+		Id:       2,
+		SongName: "She Wolf",
+		Band:     "Megadeth",
+		Artist:   "Dave Mustaine",
+		Album:    "Wryptic Writings",
+		Genre:    "Metal",
+	},
+
+	{
+		Id:       3,
+		SongName: "Half Awake",
+		Band:     "Concrete Castles",
+		Artist:   "Audra Miller",
+		Album:    "Half Awake",
+		Genre:    "Pop Rock",
+	},
+
+	{
+		Id:       4,
+		SongName: "Church",
+		Band:     "Fall Out Boy",
+		Artist:   "Patrick Stump",
+		Album:    "M A N I A",
+		Genre:    "Pop punk",
 	},
 }
 
@@ -66,8 +113,34 @@ func MenusHandler(w http.ResponseWriter, r *http.Request) {
 	menuTemplate.Execute(w, menus[menuIndex])
 }
 
+func AllSongsHandler(w http.ResponseWriter, r *http.Request) {
+	songTemplate, err := template.ParseFiles("views/songs/index.html")
+	if err != nil {
+		http.ServeFile(w, r, "public/500.html")
+		return
+	}
+	songTemplate.Execute(w, songs)
+}
+
+func SongsHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	songIndex, _ := strconv.ParseInt(params["id"], 0, 64)
+	songIndex -= 1
+
+	songTemplate, err := template.ParseFiles("views/songs/show.html")
+	if err != nil || SongOutOfRange(songIndex) {
+		http.ServeFile(w, r, "public/500.html")
+		return
+	}
+	songTemplate.Execute(w, songs[songIndex])
+}
+
 func isOutOfRange(index int64) bool {
 	return (index < 0 || index >= int64(len(menus)))
+}
+
+func SongOutOfRange(index int64) bool {
+	return (index < 0 || index >= int64(len(songs)))
 }
 
 func main() {
@@ -75,15 +148,8 @@ func main() {
 	router.HandleFunc("/home", HomeHandler)
 	router.HandleFunc("/menus", AllMenusHandler)
 	router.HandleFunc("/menus/{id:[0-9]+}", MenusHandler)
-	// Please help implement one of following routing
-	// router.HandleFunc("/products", AllProductsHandler)
-	// router.HandleFunc("/products/{id:[0-9]+}", ProductsHandler)
-	// or
-	// router.HandleFunc("/books", AllBooksHandler)
-	// router.HandleFunc("/books/{id:[0-9]+}", BooksHandler)
-	// or
-	// router.HandleFunc("/songs", AllSongsHandler)
-	// router.HandleFunc("/songs/{id:[0-9]+}", SongsHandler)
+	router.HandleFunc("/songs", AllSongsHandler)
+	router.HandleFunc("/songs/{id:[0-9]+}", SongsHandler)
 	router.NotFoundHandler = http.HandlerFunc(NotFound)
 
 	http.Handle("/", router)
